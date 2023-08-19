0efa2895b6c593426bb924365dae60be122c7bd1
karanghai9 <kg9811422163@gmail.com>
2023-08-19 18:41:10 +0200

    Initial 8*8 grid created for chessboard.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
+import Board from './Components/Board';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          Welcome to chess-game!
-        </h1>
-      </header>
+      <h1>Chess Game</h1>
+      <Board />
     </div>
   );
 }
---
+++ src/Components/Square.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Styles/Square.css';
+
+const Square = ({ row, col }) => {
+
+  return (
+    <div className={"square"}>
+      
+    </div>
+  );
+};
+
+export default Square;
