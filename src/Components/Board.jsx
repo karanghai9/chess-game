865fdd492925d235f43c48484fe5845440ffec66
karanghai9 <kg9811422163@gmail.com>
2023-08-19 19:25:36 +0200

    Flipping chessboard functionality added.

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Square from './Square';
 import '../Styles/Board.css';
 
 const Board = () => {
-  const [selectedPiece, setSelectedPiece] = useState(null);
+    const [selectedPiece, setSelectedPiece] = useState(null);
+    const [flip, setFlip] = useState(false);
+    const [boardStyling, setBoardStyling] = useState('rotateAnimationWhite 2s forwards');
 
   const [pieces, setPieces] = useState(
     [
@@ -18,27 +20,43 @@ const Board = () => {
     ]
 );
 
-  const renderSquare = (row, col, piece) => (
-    <Square
-      key={`${row}-${col}`}
-      row={row}
-      col={col}
-      piece={piece}
-    />
-  );
+    useEffect(() => {
+        if (flip === false) {
+            setBoardStyling('rotateAnimationWhite 2s forwards')
+        }
+        else setBoardStyling('rotateAnimationBlack 2s forwards')
+    }, [flip]);
 
-return (
-    <div className="board">
-      {pieces.map((row, rowIndex) => (
-        <div className="row" key={rowIndex}>
-          {row.map((piece, colIndex) =>
-            renderSquare(rowIndex, colIndex, piece)
-          )}
-        </div>
-      ))}
-    </div>
-  );
-  
+    const renderSquare = (row, col, piece) => {
+        return <Square
+            key={`${row}-${col}`}
+            row={row}
+            col={col}
+            piece={piece}
+        />
+    };
+
+    const flipBoard = () => {
+        setFlip(prevFlip => !prevFlip);
+    }
+
+    return (
+        <>
+            <div className="board" style={{ animation: boardStyling }}>
+                {pieces.map((row, rowIndex) => (
+                    <div className="row" key={rowIndex}>
+                        {row.map((piece, colIndex) =>
+                            renderSquare(rowIndex, colIndex, piece)
+                        )}
+                    </div>
+                ))}
+            </div>
+            <div className='turn'>
+                <button onClick={flipBoard} className="flipBoardButton">Flip Board</button>
+            </div>
+        </>
+    );
+
 };
 
 export default Board;
